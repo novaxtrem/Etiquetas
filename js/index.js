d79ae8053f413fecb4c7c4717ca8f4a541059877
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-12-04 14:33:41 -0300

    up

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    $("#imprimir").click(function() {
+        levantarInfoMonitorEImprimir();
+    });
+
+});
+
+function levantarInfoMonitorEImprimir() {
+    var resolucion = $("#resolucion").find('option:selected').val();
+    console.log(resolucion);
+}
