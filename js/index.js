e9ae9893c54288a175e277f132ea1418d5e5f6d1
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-12-04 14:45:03 -0300

    sd

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-
     $("#imprimir").click(function() {
         levantarInfoMonitorEImprimir();
     });
 
+
+
 });
+
 
 function levantarInfoMonitorEImprimir() {
     var resolucion = $("#resolucion").find('option:selected').val();
