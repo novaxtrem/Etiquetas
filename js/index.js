68872471ad7400d2c0c3518efd40081eda70eba9
novaxtrem <69129635+novaxtrem@users.noreply.github.com>
2020-12-11 17:11:03 -0300

    jgkgh

--- js/index.js
+++ js/index.js
@@ -1,14 +1,57 @@
+var marca = "";
+var modelo = "";
+var resolucion = "";
+var pulgadas = "";
+var precio = "";
+//
+
 document.addEventListener("DOMContentLoaded", function(event) {
     $("#imprimir").click(function() {
-        levantarInfoMonitorEImprimir();
+        marca = $("#marcaProducto").find('option:selected').val();
+        modelo = $("#modeloProducto").val();
+        resolucion = $("#resolucionProducto").find('option:selected').val();
+        pulgadas = $("#pulgadasProducto").find('option:selected').val();
+        precio = $("#precioProducto").val();
+
+        $("#portaMarcaPulgadas").text(marca + " " + pulgadas);
+        $("#portaModelo").text(modelo);
+        $("#portaResolucion").text(resolucion);
+
+        $("#portaPrecio").text(precio);
+        var divToPrint = document.getElementById('DivIdToPrint');
+
+        var newWin = window.open('', 'Print-Window');
+
+        newWin.document.open();
+
+        newWin.document.write('<html><head>    <link rel="stylesheet" href="css/index.css"></head><body onload="window.print()">' + divToPrint.innerHTML + '</body></html>');
+
+        newWin.document.close();
+
+        setTimeout(function() { newWin.close(); }, 10);
     });
-
-
-
+    //
+    $("#marcaProducto").change(function() {
+        marca = $(this).find('option:selected').val();
+        if (marca == "VARIOS") {
+            $("#modeloProducto").val("VARIOS MODELOS");
+            $("#modeloProducto").prop("disabled", true);
+        } else {
+            $("#modeloProducto").prop("disabled", false);
+            $("#modeloProducto").val("");
+        }
+    });
+    //
+    $("#modeloProducto").change(function() {
+        modelo = $(this).val();
+    });
+    //
+    $("#resolucionProducto").change(function() {
+        resolucion = $(this).find('option:selected').val();
+    });
+    //
+    $("#pulgadasProducto").change(function() {
+        pulgadas = $(this).find('option:selected').val();
+    });
+    //
 });
-
-
-function levantarInfoMonitorEImprimir() {
-    var resolucion = $("#resolucion").find('option:selected').val();
-    alert(resolucion);
-}
